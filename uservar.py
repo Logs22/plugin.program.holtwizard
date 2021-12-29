0e0d579a5968d3af649af7555128fb99e96df422
Surgeon_Turk <l22bitt1@gmail.com>
2021-12-29 12:50:12 -0500

    add build file

--- uservar.py
+++ uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR yellow][B]Holt[/B][/COLOR]Wizard'
 BUILDERNAME = 'OpenWizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'http://'
+BUILDFILE = 'https://github.com/Logs22/Builds/blob/main/builds.txt
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
